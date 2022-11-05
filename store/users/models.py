660f908ea24896cd5efa667bf53147324c34691b
Josué Santos <josue.santos.dev@gmail.com>
2022-11-04 22:29:55 -0300

    [model] (users)

--- store/users/models.py
+++ store/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+
+    id = models.AutoField(primary_key=True)
+
+    name = models.CharField(
+        max_length=256,
+        null=False,
+        blank=False)
+
+    email = models.CharField(
+        max_length=256,
+        null=False,
+        blank=False
+    )
+
+    password = models.CharField(
+        max_length=260,
+        null=False,
+        blank=False
+    )
