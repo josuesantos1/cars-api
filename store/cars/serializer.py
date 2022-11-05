6e54fd67ab98533f6826cfe9751d6b24052c0a54
Josué Santos <josue.santos.dev@gmail.com>
2022-11-04 21:58:58 -0300

    [serializer] (cars)

---
+++ store/cars/serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Cars
+
+class CarsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cars
+        fields = ["id", "name", "brand", "model", "slug", "photo"]
+
+
