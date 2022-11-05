a336b4c8c7c3739c98c5a205c224be28089f1f43
Josué Santos <josue.santos.dev@gmail.com>
2022-11-04 22:30:53 -0300

    [app] (routes) add users routes

--- store/store/urls.py
+++ store/store/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from cars import views
+from cars import views as vcar
+from users import views as vuser
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('cars/', views.cars.as_view(), name='cars'),
+    path('cars/', vcar.cars.as_view(), name='cars'),
+    path('users/', vuser.users.as_view(), name='users'),
     path('', include(router.urls))
 ]
