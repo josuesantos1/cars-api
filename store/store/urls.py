8f91725aac1752ab3133e0d38544b2e6a960f75b
Josué Santos <josue.santos.dev@gmail.com>
2022-11-07 18:25:58 -0300

    [urls] login route

--- store/store/urls.py
+++ store/store/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from cars import views as vcar
 from users import views as vuser
 
@@ -9,5 +14,6 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('cars/', vcar.cars.as_view(), name='cars'),
     path('users/', vuser.users.as_view(), name='users'),
+    path('login/', vuser.Login.as_view(), name='login'),
     path('', include(router.urls))
-]
+] 
