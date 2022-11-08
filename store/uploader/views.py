2098afaab85cb4a06e64fda83446b78a92f65655
Josué Santos <josue.santos.dev@gmail.com>
2022-11-07 21:42:02 -0300

    [VIEWS] (uploader) file upload to s3

--- store/uploader/views.py
+++ store/uploader/views.py
@@ -1,23 +1,26 @@
+import uuid, os
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, parsers
 
 from .serializer import FileSerializer
-from .models import File
 
+from boto3.session import Session
 
 class Uploader(APIView):    
     parser_classes = (parsers.MultiPartParser, parsers.FormParser)
 
     def put(self, request, format=None):
-        print(type(request.FILES['image']))
-        data = {
-            'image': request.FILES['image']
-        }
+        file_extension = os.path.splitext(str(request.FILES['image']))[1]
+        key = f"{uuid.uuid4()}{file_extension}"
 
-        serializer = FileSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
+        session = Session(region_name=str(os.getenv('REGION')),
+                          aws_access_key_id=str(os.getenv('ACCESS_KEY')),
+                          aws_secret_access_key=str(os.getenv('SECRETE_KEY')))
 
-        return Response(serializer.data)
+        s3 = session.resource('s3')
+        s3.Bucket(str(os.getenv('BUCKET'))).put_object(Key=key, Body=request.FILES['image'])
+
+        return Response({'key': key})
     
