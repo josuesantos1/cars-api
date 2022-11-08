c47a2f76328fcc08835eeac08ea4f0c287df5422
Josué Santos <josue.santos.dev@gmail.com>
2022-11-07 21:30:53 -0300

    [models] clean all

--- store/uploader/models.py
+++ store/uploader/models.py
@@ -1,19 +1,2 @@
 from django.db import models
 
-import uuid
-
-def upload_image(file, name):
-    print(file)
-    return f"{uuid.uuid4()}-{name}"
-
-class File(models.Model):
-
-    id = models.AutoField(
-        primary_key=True,
-        null=False,
-        blank=False)
-
-    image = models.ImageField(
-            upload_to=upload_image,
-            blank=True,
-            null= True)
