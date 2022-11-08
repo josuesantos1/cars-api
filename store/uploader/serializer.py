5a8ca0e6f5c78173f43b532d0a13c521f6e8601a
Josué Santos <josue.santos.dev@gmail.com>
2022-11-07 21:31:13 -0300

    [serializer] (uploader) fileField

--- store/uploader/serializer.py
+++ store/uploader/serializer.py
@@ -1,9 +1,6 @@
 from rest_framework import serializers
-from .models import File
 
 class FileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = File
-        fields = ["id", "image"]
+    file = serializers.FileField()
 
 
